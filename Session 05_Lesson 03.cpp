ef3ec9accdad985db338eb5e2d93d51b0185ef6e
Phanthuanthanh/Phanthuanthanh_ENGKS24B_Project-Flappy-bird <Phanthuanthanh270606@gmail.com>
2024-11-26 00:05:59 +0700

    Up load Bai tap Session 05_Lesson 03

---
+++ Session 05_Lesson 03.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0; 
+
+    do {
+        printf("Nhap mot so nguyen duong: ");
+        scanf("%d", &n);
+
+        if (n <= 0) {
+            printf("So phai lon hon 0. Vui long nhap lai. \n");
+        }
+    } while (n <= 0);
+
+    for (int i = 1; i <= n; i++) {
+        sum += i; 
+    }
+	printf("Tong cac so tu 1 den %d là: %d\n", n, sum);
+
+    return 0;
+}
+
+
