4c52f836d5c63ca7e13faeb3a645b42249c3fa97
Phanthuanthanh/Phanthuanthanh_ENGKS24B_Project-Flappy-bird <Phanthuanthanh270606@gmail.com>
2024-11-26 00:05:46 +0700

    Up load Bai tap Session 05_Lesson 02

---
+++ Session 05_Lesson 02.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+	int a = 27;
+	printf("Moi ban nhap vao so nguyen: ");
+	scanf("%d",&a);
+	 while(a != 27){
+	 	printf("Moi ban nhap vao so nguyen: ");
+	 	scanf("%d",&a);
+	 }
+	 return 0;
+}
